ae2a2eea7df10df94f8aed71b8c7d3dfedde2071
Christopher McCormack <mccormack.christopher@gmail.com>
2020-04-13 23:07:59 -0700

    completed counting valleys

--- hackerrank/interview_prep_kit/warm_up_challenges/counting_valleys/counting_valleys.py
+++ hackerrank/interview_prep_kit/warm_up_challenges/counting_valleys/counting_valleys.py
@@ -8,16 +8,35 @@ import sys
 
 # Complete the countingValleys function below.
 def countingValleys(n, s):
+    valleys = 0
+    level = 0
+    in_valley = False
+
+    for direction in s:
+        if direction == "U":
+            level += 1
+            if level == 0:
+                in_valley = False
+                valleys += 1
+
+        if direction == "D":
+            level -= 1
+
+        if level < 0:
+            in_valley = True
+
+    return valleys
+        
+
 
 if __name__ == '__main__':
     # fptr = open(os.environ['OUTPUT_PATH'], 'w')
 
     # n = int(input())
 
-    # s = input()
-    n = 8
-    s = "UDDDUDUU"
-    result = countingValleys(n, s)
+    
+    print(countingValleys(8, "UDDDUDUU"))
+    print(countingValleys(8, "DUDUDU"))
 
     # fptr.write(str(result) + '\n')
 

5bb024934bba040c9ded527afd8e5706100590f6
cmccormack <mack.developer@gmail.com>
2020-04-14 18:51:59 -0700

    installed pylint, completed jumping_on_clouds

--- hackerrank/interview_prep_kit/warm_up_challenges/jumping_on_the_clouds/jumping_on_clouds.py
+++ hackerrank/interview_prep_kit/warm_up_challenges/jumping_on_the_clouds/jumping_on_clouds.py
@@ -8,16 +8,22 @@ import sys
 
 # Complete the jumpingOnClouds function below.
 def jumpingOnClouds(c):
+    clen = len(c)
+    i = 0
+    jumps = 0
+    while (i < clen - 1):
+        i += 1
+        if (i+1) < clen and c[i+1] == 0:
+            i+=1
+
+        jumps += 1
+
+    return jumps
+
 
 if __name__ == '__main__':
-    fptr = open(os.environ['OUTPUT_PATH'], 'w')
-
-    n = int(input())
-
-    c = list(map(int, input().rstrip().split()))
-
-    result = jumpingOnClouds(c)
-
-    fptr.write(str(result) + '\n')
-
-    fptr.close()
+    assert jumpingOnClouds([0,0,1,0,0,1,0]) == 4
+    assert jumpingOnClouds([0,0,0,1,0,0]) == 3
+    assert jumpingOnClouds([0,0,0,0,1,0]) == 3
+    assert jumpingOnClouds([0,1,0,0,0,1,0,0,1,0]) == 5
+    assert jumpingOnClouds([0,1,0,0,0,1,0,0,1,0,1,0]) == 6

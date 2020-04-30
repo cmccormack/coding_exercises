79e1b9a97a0b4872a4ae93b7fe7649a158e00b89
cmccormack <mack.developer@gmail.com>
2020-04-30 16:40:10 -0700

    updated

--- utils/buildBinaryTreeFromArray.js
+++ utils/buildBinaryTreeFromArray.js
@@ -4,22 +4,29 @@ const TreeNode = function (val, left, right) {
   this.right = right === undefined ? null : right;
 };
 
+const debugMode = false;
+
+const debug = (...args) => {
+  if (debugMode) console.debug(`[BuildTree] `, ...args);
+};
+
 const buildBinaryTreeFromArray = function (arr) {
+  debug(JSON.stringify(arr), arr.length);
   const root = new TreeNode(arr[0]);
   arr[0] = root;
   for (let i = 0; i < arr.length; i += 1) {
     let current = arr[i];
-    // console.log(current || "null");
-    if (!current) continue;
+    debug(`i: ${i} arr[i]: ${JSON.stringify(arr[i])}`);
+    if (current === null) continue;
     const left = 2 * i + 1;
     const right = 2 * i + 2;
-    if (arr[left]) {
-      // console.log(`arr[${left}] found (left): ${arr[left]}`);
+    if (arr[left] < arr.length && arr[left] !== null) {
+      debug(`i: ${i} left child found at arr[${left}]: ${arr[left]}`);
       arr[left] = new TreeNode(arr[left]);
       current.left = arr[left];
     }
-    if (arr[right]) {
-      // console.log(`arr[${right}] found (right): ${arr[right]}`);
+    if (arr[right] < arr.length && arr[right] !== null) {
+      debug(`i: ${i} right child found at arr[${right}]: ${arr[right]}`);
       arr[right] = new TreeNode(arr[right]);
       current.right = arr[right];
     }

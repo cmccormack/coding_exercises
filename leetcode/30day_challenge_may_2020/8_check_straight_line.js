c3adf171564776136d996c99c8991055b4c90ce9
Christopher McCormack <mccormack.christopher@gmail.com>
2020-05-08 09:21:45 -0700

    initial commit

---
+++ leetcode/30day_challenge_may_2020/8_check_straight_line.js
@@ -0,0 +1,27 @@
+const timeit = require("../../utils/timeit");
+
+/**
+ * @param {number[][]} coordinates
+ * @return {boolean}
+ */
+var checkStraightLine = function (coordinates) {
+  if (coordinates.length === 1) return true;
+};
+
+timeit(checkStraightLine, true, [
+  [1, 2],
+  [2, 3],
+  [3, 4],
+  [4, 5],
+  [5, 6],
+  [6, 7],
+]);
+timeit(checkStraightLine, false, [
+  [1, 1],
+  [2, 2],
+  [3, 4],
+  [4, 5],
+  [5, 6],
+  [7, 7],
+]);
+timeit(checkStraightLine, true, [[1, 1]]);

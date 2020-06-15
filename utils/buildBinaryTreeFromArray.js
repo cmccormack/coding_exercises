a18dc8f4d417b8d0a39347631310fe5052d5648c
Christopher McCormack <mack.developer@gmail.com>
2020-06-15 08:39:44 -0700

    updated

--- utils/buildBinaryTreeFromArray.js
+++ utils/buildBinaryTreeFromArray.js
@@ -10,33 +10,33 @@ const debug = (...args) => {
   if (debugMode) console.debug(`[BuildTree] `, ...args);
 };
 
-const buildBinaryTreeFromArray_old = function (arr) {
-  debug(JSON.stringify(arr), arr.length);
-  const root = new TreeNode(arr[0]);
-  arr[0] = root;
-  for (let i = 0; i < arr.length; i += 1) {
-    let current = arr[i];
-    debug(`i: ${i} arr[i]: ${JSON.stringify(arr[i])}`);
-    if (current === null) continue;
-    const left = 2 * i + 1;
-    const right = 2 * i + 2;
-    if (arr[left] < arr.length && arr[left] !== null) {
-      debug(`i: ${i} left child found at arr[${left}]: ${arr[left]}`);
-      arr[left] = new TreeNode(arr[left]);
-      current.left = arr[left];
-    }
-    if (arr[right] < arr.length && arr[right] !== null) {
-      debug(`i: ${i} right child found at arr[${right}]: ${arr[right]}`);
-      arr[right] = new TreeNode(arr[right]);
-      current.right = arr[right];
-    }
-  }
+// const buildBinaryTreeFromArray_old = function (arr) {
+//   debug(JSON.stringify(arr), arr.length);
+//   const root = new TreeNode(arr[0]);
+//   arr[0] = root;
+//   for (let i = 0; i < arr.length; i += 1) {
+//     let current = arr[i];
+//     debug(`i: ${i} arr[i]: ${JSON.stringify(arr[i])}`);
+//     if (current === null) continue;
+//     const left = 2 * i + 1;
+//     const right = 2 * i + 2;
+//     if (arr[left] < arr.length && arr[left] !== null) {
+//       debug(`i: ${i} left child found at arr[${left}]: ${arr[left]}`);
+//       arr[left] = new TreeNode(arr[left]);
+//       current.left = arr[left];
+//     }
+//     if (arr[right] < arr.length && arr[right] !== null) {
+//       debug(`i: ${i} right child found at arr[${right}]: ${arr[right]}`);
+//       arr[right] = new TreeNode(arr[right]);
+//       current.right = arr[right];
+//     }
+//   }
 
-  return root;
-};
+//   return root;
+// };
 
 // Using recursive approach
-const buildBinaryTreeFromArray = (arr, root, i) => {
+const buildBinaryTreeFromArray = (arr, root = null, i = 0) => {
   if (i < arr.length) {
     root = new TreeNode(arr[i]);
 
@@ -47,32 +47,32 @@ const buildBinaryTreeFromArray = (arr, root, i) => {
   return root;
 };
 
-const printInOrder = (root) => {
-  if (root !== null) {
-    printInOrder(root.left);
-    console.log(`${root.val} `);
-    printInOrder(root.right);
-  }
-};
-const printPreOrder = (root) => {
-  let output = "";
+// const printInOrder = (root) => {
+//   if (root !== null) {
+//     printInOrder(root.left);
+//     console.log(`${root.val} `);
+//     printInOrder(root.right);
+//   }
+// };
+// const printPreOrder = (root) => {
+//   let output = "";
 
-  (function preOrder(root) {
-    if (root !== null) {
-      output += `${root.val} `;
-      preOrder(root.left);
-      preOrder(root.right);
-    }
-  })(root);
+//   (function preOrder(root) {
+//     if (root !== null) {
+//       output += `${root.val} `;
+//       preOrder(root.left);
+//       preOrder(root.right);
+//     }
+//   })(root);
 
-  console.log(output);
-};
+//   console.log(output);
+// };
 
-const res = buildBinaryTreeFromArray(
-  [1, 2, 3, 4, 5, 6, 6, 6, 6, 6, null, null],
-  null,
-  0
-);
+// const res = buildBinaryTreeFromArray(
+//   [1, 2, 3, 4, 5, 6, 6, 6, 6, 6, null, null],
+//   null,
+//   0
+// );
 // console.log(res);
 exports.TreeNode = TreeNode;
 exports.buildBinaryTreeFromArray = buildBinaryTreeFromArray;
--- utils/timeit.js
+++ utils/timeit.js
@@ -1,10 +1,16 @@
 function timeit(func, expected, ...args) {
   const br = `\n---------------------------------------------\n`;
   if (Array.isArray(expected)) expected = JSON.stringify(expected);
+  if (typeof expected === "object" && expected !== null) {
+    expected = JSON.stringify(expected);
+  }
   const label = `   RESULT EXPECTED: ${expected}\t\tTime`;
   console.time(label);
   result = func(...args);
   if (Array.isArray(result)) result = JSON.stringify(result);
+  if (typeof result === "object" && result !== null) {
+    result = JSON.stringify(result);
+  }
   const icon = result === expected ? "🎉" : "❌";
   console.log(`${br}${icon} RESULT RETURNED: ${result}`);
   console.timeEnd(label);
